28a4b8b9391c1f3173f09ebd940a3badfe5727b2
“raphnw” <raphael.nwaokolo@gmail.com>
2024-09-30 13:07:50 -0600

    update files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "s3-vpro-gitop-200"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
